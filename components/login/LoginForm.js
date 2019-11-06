c87545756c9c8c1aeeefd1f3c406ad86832131fd
Yao Lei <yao_lei_java@hotmail.com>
2019-11-06 09:35:00 +0800

    add antd css to project

--- components/login/LoginForm.js
+++ components/login/LoginForm.js
@@ -1,9 +1,4 @@
-// import { Form, Icon, Input, Button, Checkbox } from 'antd';
-import Form from 'antd/lib/form'
-import Icon from 'antd/lib/icon'
-import Input from 'antd/lib/input'
-import Button from 'antd/lib/button'
-import Checkbox from 'antd/lib/checkbox'
+import { Form, Input, Row, Col } from 'antd'
 import PromptBox from '../../components/PromptBox'
 import {randomNum, calculateWidth} from '../../untils/utils'
 import 'antd/dist/antd.css'
@@ -21,12 +16,53 @@ class LoginFormBase extends React.Component {
     });
   };
 
+  componentDidMount () {
+    this.createCode()
+  }
+  
+  /** 
+  * 生成验证码
+  */
+ createCode = () => {
+   const ctx = this.canvas.getContext('2d')
+   const chars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+   let code = ''
+   ctx.clearRect(0, 0, 80, 39)
+   for (let i = 0; i < 4; i++) {
+     const char = chars[randomNum(0, 57)]
+     code += char
+     ctx.font = randomNum(20, 25) + 'px SimHei'  //设置字体随机大小
+     ctx.fillStyle = '#D3D7F7'
+     ctx.textBaseline = 'middle'
+     ctx.shadowOffsetX = randomNum(-3, 3)
+     ctx.shadowOffsetY = randomNum(-3, 3)
+     ctx.shadowBlur = randomNum(-3, 3)
+     ctx.shadowColor = 'rgba(0, 0, 0, 0.3)'
+     let x = 80 / 5 * (i + 1)
+     let y = 39 / 2
+     let deg = randomNum(-25, 25)
+     /**设置旋转角度和坐标原点**/
+     ctx.translate(x, y)
+     ctx.rotate(deg * Math.PI / 180)
+     ctx.fillText(char, 0, 0)
+     /**恢复旋转角度和坐标原点**/
+     ctx.rotate(-deg * Math.PI / 180)
+     ctx.translate(-x, -y)
+   }
+   this.setState({
+     code
+   })
+ }
+
+
   render() {
     const { getFieldDecorator, getFieldError } = this.props.form;
     const {focusItem, code} = this.state
     return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-                <Form.Item help={getFieldError('username') &&
+      <div className={this.props.className}>
+        <h3 className='title'>管理员登录</h3>
+        <Form onSubmit={this.loginSubmit}>
+          <Form.Item help={getFieldError('username') &&
           <PromptBox info={getFieldError('username')} width={calculateWidth(getFieldError('username'))}/>}>
             {getFieldDecorator('username', {
               rules: [{required: true, message: '请输入用户名'}]
@@ -36,47 +72,72 @@ class LoginFormBase extends React.Component {
                 onBlur={() => this.setState({focusItem: -1})}
                 maxLength={16}
                 placeholder='用户名'
-                />
+                addonBefore={<span className='iconfont icon-User' style={focusItem === 0 ? styles.focus : {}}/>}/>
             )}
           </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Username"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="Password"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(<Checkbox>Remember me</Checkbox>)}
-          <a className="login-form-forgot" href="">
-            Forgot password
-          </a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
-        </Form.Item>
-      </Form>
+          <Form.Item help={getFieldError('password') &&
+          <PromptBox info={getFieldError('password')} width={calculateWidth(getFieldError('password'))}/>}>
+            {getFieldDecorator('password', {
+              rules: [{required: true, message: '请输入密码'}]
+            })(
+              <Input
+                onFocus={() => this.setState({focusItem: 1})}
+                onBlur={() => this.setState({focusItem: -1})}
+                type='password'
+                maxLength={16}
+                placeholder='密码'
+                addonBefore={<span className='iconfont icon-suo1' style={focusItem === 1 ? styles.focus : {}}/>}/>
+            )}
+          </Form.Item>
+          <Form.Item help={getFieldError('verification') &&
+          <PromptBox info={getFieldError('verification')} width={calculateWidth(getFieldError('verification'))}/>}>
+            {getFieldDecorator('verification', {
+              validateFirst: true,
+              rules: [
+                {required: true, message: '请输入验证码'},
+                {
+                  validator: (rule, value, callback) => {
+                    if (value.length >= 4 && code.toUpperCase() !== value.toUpperCase()) {
+                      callback('验证码错误')
+                    }
+                    callback()
+                  }
+                }
+              ]
+            })(
+              <Row>
+                <Col span={15}>
+                  <Input
+                    onFocus={() => this.setState({focusItem: 2})}
+                    onBlur={() => this.setState({focusItem: -1})}
+                    maxLength={4}
+                    placeholder='验证码'
+                    addonBefore={<span className='iconfont icon-securityCode-b'
+                                       style={focusItem === 2 ? styles.focus : {}}/>}/>
+                </Col>
+                <Col span={9}>
+                  <canvas onClick={this.createCode} width="80" height='39' ref={el => this.canvas = el}/>
+                </Col>
+              </Row>
+            )}
+          </Form.Item>
+          <div className='bottom'>
+            <input className='loginBtn' type="submit" value='登录'/>
+            <span className='registerBtn' onClick={this.register}>注册</span>
+          </div>
+        </Form>
+        <div className='footer'>
+          <div>欢迎登陆后台管理系统</div>
+        </div>
+      </div>
     );
   }
 }
-
+const styles = {
+  focus: {
+    width: '20px',
+    opacity: 1
+  },
+}
 const LoginForm = Form.create({})(LoginFormBase);
 export default LoginForm;
--- pages/admin.js
+++ pages/admin.js
@@ -1,5 +1,7 @@
 import LoginForm from '../components/login/LoginForm'
+import '../components/login/style.css'
 import Link from 'next/link'
+import 'animate.css'
 class admin extends React.Component {
 
     state = {
@@ -15,34 +17,55 @@ class admin extends React.Component {
     render () {
         const {title} = this.state;
         return (
-            <div>
-                <Link href="/">
-                    <a> Back to home </a>
-                </Link>
-                <input value={this.state.title} onChange={this.changeTitle.bind(this)}></input>
-                <LoginForm title={title} />
+            <div id='login-page'>
+                <div>
+                <h3 style={styles.loadingTitle} className='animated bounceInLeft'>载入中...</h3>
+                </div>:
+                <div>
+                  <div id='backgroundBox' style={styles.backgroundBox}/>
+                  <div className='container'>
+                    <LoginForm />
+                  </div>
+                </div>
             </div>
         )
     }
 }
 
+const styles = {
+    backgroundBox: {
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      width: '100vw',
+      height: '100vh',
+      // backgroundImage: 'url(https://github.com/zhangZhiHao1996/image-store/blob/master/react-admin-master/bg5.jpg?raw=true)',
+      backgroundImage: 'url(https://github.com/zhangZhiHao1996/image-store/blob/master/react-admin-master/bg1.jpg?raw=true)',
+      backgroundSize: '100% 100%',
+      transition:'all .5s'
+    },
+    focus: {
+      // transform: 'scale(0.7)',
+      width: '20px',
+      opacity: 1
+    },
+    loadingBox:{
+      position:'fixed',
+      top:'50%',
+      left:'50%',
+      transform:'translate(-50%,-50%)'
+    },
+    loadingTitle:{
+      position:'fixed',
+      top:'50%',
+      left:'50%',
+      marginLeft: -45,
+      marginTop: -18,
+      color:'#000',
+      fontWeight:500,
+      fontSize:24
+    },
+  }
+
 export default admin;
 
-
-
-// import { useRouter } from 'next/router'
-// import Link from 'next/link'
-// import LoginForm from '../components/login/LoginForm'
-// const Admin = () => {
-//   const router = useRouter()
-//   const { slug } = router.query
-//   return (
-//         <div>
-//         <Link href="/">
-//            <a> Back to home </a>
-//         </Link>
-//         <LoginForm />
-//         </div>
-//         )
-// }
-// export default Admin
